0c7247b63524f87dd6690b9cf9fb1f5c0ffba529
merbng <1146797082@qq.com>
2017-11-06 23:54:00 +0800

    No.1

---
+++ test.py
@@ -0,0 +1,16 @@
+#-*- coding: UTF-8 -*-
+print ("哈哈哈");
+''''多行注释，使用单引号'''
+"""多行注释，，使用双引号"""
+#raw_input("\n\n执行后就会等待用户输入，按回车键后就会退出.\n");
+
+#import sys;x='runoob';sys.stdout.write(x+'\n');
+#不换行输出
+x="a";
+print x,
+#不换行输出
+y='b';
+print y;
+#2017年11月6日23:51:34
+#http://www.runoob.com/python/python-basic-syntax.html
+#多个语句构成代码组
