5a79960c447385ba382605af2b7a970ce97b9eb1
merbng <1146797082@qq.com>
2017-11-18 21:08:31 +0800

    No.12

--- study.py
+++ study.py
@@ -1118,25 +1118,53 @@ import os
 # 触发异常
 # http://www.runoob.com/python/python-exceptions.html
 
+class Vector:
+    def __init__(self,a,b):
 
+        self.a=a
+        self.b=b
 
+    def __str__(self):
+        return "Vector (%d,%d)"%(self.a,self.b)
 
+    def __add__(self, other):
+        return Vector(self.a+other.a,self.b+other.b)
 
+v1 =Vector(2,10)
+v2 =Vector(5,-2)
+print v1+v2
 
+# 类属性与方法
+# 类的私有属性
+# __private_attrs:两个下划线开头，声明该属性为私有，不能在类的外部使用或直接访问
+# 在类内部的方法中使用时，self.__private_attrs
+# 类的方法
+# 在类的内部，使用def关键字可以为类定义一个方法，与一般函数定义不同，不能在类的外部调用，在类的
+# 内部调用self.__private_methods
+#
+class JustCounter:
+    __secretCount =0#私有变量
+    publicCount =0#公开变量
 
+    def count(self):
+        self.__secretCount+=1
+        self.publicCount+=1
+        print self.__secretCount
 
+counter =JustCounter()
+counter.count()
+counter.count()
+print counter.publicCount
+# print counter.__secretCount#报错。不能访问私有变量
+# print counter._JustCounter__secretCount
 
+# 单下划线、双下划线、头尾双下划线说明：
+# __foo__：定义的是特殊方法，一般是系统定义名字，类似__init()__之类的
+# _foo：以单下划线开头表示protected类型的变量，即保护类型只能允许其本身与子类进行访问，不能用于from module import*
+# __foo:双下划线的表示的是私有类型（private）的变量，只能是允许这个类本身进行访问了
 
-
-
-
-
-
-
-
-
-
-
-
+# Python正则表达式
+# 2017-11-18 21:07:51
+# http://www.runoob.com/python/python-reg-expressions.html
 
 
